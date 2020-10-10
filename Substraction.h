89952d5a37e8de5ae1f118f960192153409c8873
popolito1 <60854956+popolito1@users.noreply.github.com>
2020-10-10 19:13:18 +0200

    Update Substraction.h

--- Substraction.h
+++ Substraction.h
@@ -10,6 +10,9 @@
 
 #include <stdio.h>
 // FP2.1 : HugeNumberCalculator must allow subtraction between two signed integers without limit of number of digits.
+int substraction_signed(int,int);
 // FP2.2 : HugeNumberCalculator must allow subtraction between two unsigned integers without limit of number of digits.
+unsigned int substraction_unsigned(unsigned int, unsigned int);
 // FP2.3 : HugeNumberCalculator must allow subtraction between two signed or unsigned real numbers with no limit on the number of digits.
+float substraction_real(float,float);
 #endif /* Substraction_h */
