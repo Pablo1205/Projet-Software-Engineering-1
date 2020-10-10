afec6eece7954550884a828426457238267dc551
popolito1 <60854956+popolito1@users.noreply.github.com>
2020-10-10 19:11:01 +0200

    Update Multiplication.h

--- Multiplication.h
+++ Multiplication.h
@@ -10,6 +10,9 @@
 
 #include <stdio.h>
 // FP3.1 : HugeNumberCalculator must be able to multiply between two signed integers without limit of number of digits.
+int multiplication_signed (int,int);
 // FP3.2 : HugeNumberCalculator must be able to perform a multiplication between two unsigned integers without limit of number of digits.
+unsigned int multiplication_unsigned (unsigned int,unsigned int);
 // FP3.3 : HugeNumberCalculator must be able to perform a multiplication between two signed or unsigned integer real numbers without limit of number of digits.
+float multiplication_real(float,float);
 #endif /* Multiplication_h */
