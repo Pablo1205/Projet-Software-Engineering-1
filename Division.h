0e2cacf2ff20e27e922ebbb375dd95c011e978b8
popolito1 <60854956+popolito1@users.noreply.github.com>
2020-10-10 19:14:48 +0200

    Update Division.h

--- Division.h
+++ Division.h
@@ -12,5 +12,5 @@
 // FP4.1 : HugeNumberCalculator must be able to perform a Euclidean division between two signed integers without limit of number of digits.
 int division_signed (int,int);
 // FP4.2 : HugeNumberCalculator must be able to perform a Euclidean division between two unsigned integers without limit of number of digits.
-int division_unsgined (unsigned int, unsigned int);
+unsigned int division_unsgined (unsigned int, unsigned int);
 #endif /* Division_h */
